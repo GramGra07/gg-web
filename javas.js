9c49dfecad6a9624db79e4c1d00597d1cd89cfe7
Graden <101433010+GramGra07@users.noreply.github.com>
2023-12-31 22:51:36 -0700

    updates

--- javas.js
+++ javas.js
@@ -1,97 +1,162 @@
-function toRun(){
-document.addEventListener("DOMContentLoaded", function () {
-  // Select all sections with the class 'active'
-  const sections = document.querySelectorAll(".active");
+function toRun() {
+    document.addEventListener("DOMContentLoaded", function () {
+        // Select all sections with the class 'active'
+        const sections = document.querySelectorAll(".active");
 
-  // Function to toggle the 'active' class
-  function toggleActiveSections() {
-      sections.forEach((section) => {
-          section.classList.toggle("active");
-      });
-  }
-});
+        // Function to toggle the 'active' class
+        function toggleActiveSections() {
+            sections.forEach((section) => {
+                section.classList.toggle("active");
+            });
+        }
+    });
 
-particlesJS("particles-js", {
-  particles: {
-      number: {
-          value: 80,
-          density: { enable: true, value_area: 800 },
-      },
-      color: { value: "#ffffff" },
-      shape: {
-          type: "circle",
-          stroke: { width: 0, color: "#000000" },
-          polygon: { nb_sides: 5 },
-          image: {
-              src: "img/github.svg",
-              width: 100,
-              height: 100,
-          },
-      },
-      opacity: {
-          value: 0.5,
-          random: false,
-          anim: {
-              enable: false,
-              speed: 1,
-              opacity_min: 0.1,
-              sync: false,
-          },
-      },
-      size: {
-          value: 3,
-          random: true,
-          anim: {
-              enable: false,
-              speed: 40,
-              size_min: 0.1,
-              sync: false,
-          },
-      },
-      line_linked: {
-          enable: true,
-          distance: 150,
-          color: "#ffffff",
-          opacity: 0.4,
-          width: 1,
-      },
-      move: {
-          enable: true,
-          speed: 6,
-          direction: "none",
-          random: false,
-          straight: false,
-          out_mode: "out",
-          bounce: false,
-          attract: { enable: false, rotateX: 600, rotateY: 1200 },
-      },
-  },
-  interactivity: {
-      detect_on: "canvas",
-      events: {
-          onhover: { enable: true, mode: "repulse" },
-          onclick: { enable: true, mode: "push" },
-          resize: true,
-      },
-      modes: {
-          grab: { distance: 400, line_linked: { opacity: 1 } },
-          bubble: {
-              distance: 200,
-              size: 20,
-              duration: 2,
-              opacity: 8,
-              speed: 3,
-          },
-          repulse: { distance: 100, duration: 0.4 },
-          push: { particles_nb: 4 },
-          remove: { particles_nb: 2 },
-      },
-  },
-  retina_detect: true,
-});
+    // Replace these dates with the start dates of your coding experience
+    const htmlStartDate = "2023-01-01";
+    const dartStartDate = "2023-05-01";
+    const javaStartDate = "2021-09-09";
+    const pythonStartDate = "2019-07-01";
+    const markdownStartDate = "2022-08-02";
+    const cplusStartDate = "2022-08-14"
+    const swiftStartDate = "2023-10-16"
 
-update = function () {
-  requestAnimationFrame(update);
-};
-requestAnimationFrame(update);
+    const startDateMap = {
+        dart: dartStartDate,
+        python: pythonStartDate,
+        java: javaStartDate,
+        html: htmlStartDate,
+        markdown: markdownStartDate,
+        cplus: cplusStartDate,
+        swift: swiftStartDate,
+    };
+    const languages = Object.keys(startDateMap);
+
+    const today = new Date();
+    let longestLanguage = "";
+    let longestTime = 0;
+
+    for (const [language, startDateStr] of Object.entries(startDateMap)) {
+        const startDate = new Date(startDateStr);
+        const timeDifference = today.getTime() - startDate.getTime();
+        if (timeDifference > longestTime) {
+            longestLanguage = language;
+            longestTime = timeDifference;
+        }
+    }
+    const longestStartDate = startDateMap[longestLanguage];
+    const longestStart = new Date(longestStartDate);
+    const currentDate = new Date();
+    const totalExperience = currentDate - longestStart;
+
+    function calculateProgress(startDate) {
+        const start = new Date(startDate);
+        const currentExperience = currentDate - start;
+        const progress = (currentExperience / totalExperience) * 100;
+        return progress; 
+    }
+
+    function updateProgressBar(language, startDate) {
+        const progressBar = document.getElementById(`${language}ProgressBar`);
+        const progress = calculateProgress(startDate, language);
+        const currentExperience = (
+            (((progress / 100) * totalExperience) / 86400000) / 365
+        ).toFixed(2);
+
+        progressBar.style.width = `${progress}%`;
+        // set the document o f language
+        ti = document.getElementById(language);
+        lang = language.charAt(0).toUpperCase() + language.slice(1);
+        if (lang == 'Cplus'){
+            lang = 'C++'
+        }
+        ti.innerHTML = `${lang}, Years: ${currentExperience}`;
+    }
+
+    languages.forEach((language) => {
+        updateProgressBar(language, startDateMap[language]);
+    });
+
+    particlesJS("particles-js", {
+        particles: {
+            number: {
+                value: 80,
+                density: { enable: true, value_area: 800 },
+            },
+            color: { value: "#ffffff" },
+            shape: {
+                type: "circle",
+                stroke: { width: 0, color: "#000000" },
+                polygon: { nb_sides: 5 },
+                image: {
+                    src: "img/github.svg",
+                    width: 100,
+                    height: 100,
+                },
+            },
+            opacity: {
+                value: 0.5,
+                random: false,
+                anim: {
+                    enable: false,
+                    speed: 1,
+                    opacity_min: 0.1,
+                    sync: false,
+                },
+            },
+            size: {
+                value: 3,
+                random: true,
+                anim: {
+                    enable: false,
+                    speed: 40,
+                    size_min: 0.1,
+                    sync: false,
+                },
+            },
+            line_linked: {
+                enable: true,
+                distance: 150,
+                color: "#ffffff",
+                opacity: 0.4,
+                width: 1,
+            },
+            move: {
+                enable: true,
+                speed: 6,
+                direction: "none",
+                random: false,
+                straight: false,
+                out_mode: "out",
+                bounce: false,
+                attract: { enable: false, rotateX: 600, rotateY: 1200 },
+            },
+        },
+        interactivity: {
+            detect_on: "canvas",
+            events: {
+                onhover: { enable: true, mode: "repulse" },
+                onclick: { enable: true, mode: "push" },
+                resize: true,
+            },
+            modes: {
+                grab: { distance: 400, line_linked: { opacity: 1 } },
+                bubble: {
+                    distance: 200,
+                    size: 20,
+                    duration: 2,
+                    opacity: 8,
+                    speed: 3,
+                },
+                repulse: { distance: 100, duration: 0.4 },
+                push: { particles_nb: 4 },
+                remove: { particles_nb: 2 },
+            },
+        },
+        retina_detect: true,
+    });
+
+    update = function () {
+        requestAnimationFrame(update);
+    };
+    requestAnimationFrame(update);
 }
